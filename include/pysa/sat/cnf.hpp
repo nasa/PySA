a07e2a197e74ba295c5db6b15393eb2406f99daa
Humberto Munoz-Bauza <humberto.munozb@gmail.com>
2024-05-28 14:39:12 -0700

    Separate sat.hpp header (#10)
    * refactor sat.hpp header
    
    * run clang-format

---
+++ include/pysa/sat/cnf.hpp
@@ -0,0 +1,128 @@
+/*
+Author: Salvatore Mandra (salvatore.mandra@nasa.gov)
+
+Copyright © 2023, United States Government, as represented by the Administrator
+of the National Aeronautics and Space Administration. All rights reserved.
+
+The PySA, a powerful tool for solving optimization problems is licensed under
+the Apache License, Version 2.0 (the "License"); you may not use this file
+except in compliance with the License. You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0.
+
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+*/
+#pragma once
+
+#include <iostream>
+#include <optional>
+#include <random>
+#include <regex>
+#include <sstream>
+#include <vector>
+
+#ifdef USE_MPI
+#include <mpi.h>
+#endif
+
+namespace pysa::dpll::sat {
+
+template <typename InputStream, typename ClauseType = std::vector<int32_t>,
+          template <typename...> typename Vector = std::vector>
+auto ReadCNF(InputStream &&input_stream) {
+  // Initialize formula
+  Vector<ClauseType> formula;
+
+  // Initialize current clause
+  ClauseType current_cl;
+
+  bool parsed_problem_line = false;
+  std::size_t line_num = 0;
+  std::string line;
+
+  // For each line in input ...
+  while (std::getline(input_stream, line)) {
+    // Increment line number
+    ++line_num;
+
+    // If line is empty, skip
+    if (line.length() == 0)
+      continue;
+
+    // If line is a comment, skip
+    if (std::tolower(line[0]) == 'c')
+      continue;
+
+    // If line is problem line, skip
+    if (std::regex_match(line, std::regex(R"(^p\s+cnf\s+\d+\s+\d+\s*)"))) {
+      if (parsed_problem_line) {
+        std::cerr << "Problem line parsed multiple times" << std::endl;
+        throw std::runtime_error("CNF Parse Error");
+      }
+      parsed_problem_line = true;
+
+#ifndef NDEBUG
+      std::cerr << line << std::endl;
+#endif
+      continue;
+    }
+
+    // Check if problem line appears exactly once
+    if (!parsed_problem_line) {
+      std::cerr << "CNF file in the wrong format" << std::endl;
+      throw std::runtime_error("CNF Parse Error");
+    }
+
+    // Check that line is a sequence of numbers
+    if (!std::regex_match(line,
+                          std::regex(R"(^\s*-?\d+(\s+-?\d+)*\s+0\s*$)"))) {
+      std::cerr << "Failed to parse line " << line_num << ": " << line << ".";
+      throw std::runtime_error("CNF Parse Error");
+    }
+
+    //  Parse lines
+    {
+      std::stringstream ss(line);
+      typename ClauseType::value_type js;
+
+      // For each token in line ...
+      while (ss >> js)
+
+        // If token is different from zero, append to current clause
+        if (js != 0)
+          current_cl.push_back(js);
+
+        // Otherwise, append to formula
+        else {
+          formula.push_back(std::move(current_cl));
+
+          // Remove all elements from current_cl
+          // (in case moving didn't reset it)
+          current_cl.clear();
+
+          // Ignore everything after zero
+          break;
+        }
+
+      // Only spaces and non-printable characters should remain
+      {
+        std::string left_;
+        ss >> left_;
+        if (!ss.eof() ||
+            std::any_of(std::begin(left_), std::end(left_), [](auto &&c) {
+              return !std::isspace(c) && std::isprint(c);
+            })) {
+          std::cerr << "Failed to parse line " << line_num << ": " << line
+                    << ".";
+          throw std::runtime_error("CNF Parse Error");
+        }
+      }
+    }
+  }
+
+  // Return formula
+  return formula;
+}
+} // namespace pysa::dpll::sat
---
+++ include/pysa/sat/instance.hpp
@@ -0,0 +1,203 @@
+/*
+Author: Salvatore Mandra (salvatore.mandra@nasa.gov)
+
+Copyright © 2023, United States Government, as represented by the Administrator
+of the National Aeronautics and Space Administration. All rights reserved.
+
+The PySA, a powerful tool for solving optimization problems is licensed under
+the Apache License, Version 2.0 (the "License"); you may not use this file
+except in compliance with the License. You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0.
+
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+*/
+#pragma once
+
+#include <algorithm>
+#include <iostream>
+#include <optional>
+#include <random>
+#include <vector>
+
+#include "../bitset/bitset.hpp"
+
+#ifdef USE_MPI
+#include <mpi.h>
+#endif
+
+namespace pysa::dpll::sat {
+
+auto GetRandomInstance(const std::size_t k, const std::size_t n,
+                       const std::size_t m,
+                       const std::optional<std::size_t> seed = std::nullopt) {
+  /*
+   * Generate k-SAT random instance.
+   */
+  using clause_type = std::vector<int>;
+
+  // Check
+  if (n < k)
+    throw std::runtime_error("'n' must be larger than 'k'");
+
+  // Get random seed
+  const std::size_t seed_ = seed.value_or(std::random_device()());
+
+  // Initialize random generator
+  std::mt19937_64 rng_(seed_);
+
+#ifndef NDEBUG
+  std::cerr << "# Used seed: " << seed_ << std::endl;
+#endif
+
+  // Initialize set of indexes
+  std::vector<int> indexes_(n);
+  for (std::size_t i_ = 0; i_ < n; ++i_)
+    indexes_[i_] = i_ + 1;
+
+  // Generate single clause
+  auto get_clause_ = [&rng_, &indexes_, k]() {
+    // Initialize clause
+    clause_type clause_;
+
+    // Shuffle list of indexes
+    std::shuffle(std::begin(indexes_), std::end(indexes_), rng_);
+
+    // Update clause
+    std::transform(std::begin(indexes_), std::begin(indexes_) + k,
+                   std::back_inserter(clause_),
+                   [&rng_](auto &&x) { return (rng_() % 2 ? 1 : -1) * x; });
+
+    // Return clauses
+    return clause_;
+  };
+
+  // Initialize clauses
+  std::vector<clause_type> clauses_;
+  std::generate_n(std::back_inserter(clauses_), m, get_clause_);
+  return clauses_;
+}
+
+template <typename Formula> auto GetNVars(Formula &&formula) {
+  std::size_t max_ = 0;
+  for (const auto &clause_ : formula)
+    for (const auto &x_ : clause_) {
+      // All variables should be different from zero
+      if (x_ == 0)
+        throw std::runtime_error("All variables should be different from zero");
+      if (const std::size_t a_ = std::abs(x_); a_ > max_)
+        max_ = a_;
+    }
+  return max_;
+}
+
+template <typename BitSet = BitSet<>,
+          template <typename...> typename Vector = std::vector>
+struct Instance {
+  // VarIndex -> ClauseIndex
+  const Vector<Vector<std::size_t>> clauses;
+
+  // VarIndex -> VarSign in Clause
+  const Vector<BitSet> signs;
+
+  // ClauseIndex -> VarIndex
+  const Vector<std::size_t> last_var;
+
+  template <typename Clauses, typename Signs, typename LastVar>
+  Instance(Clauses &&clauses, Signs &&signs, LastVar &&last_var)
+      : clauses{std::forward<Clauses>(clauses)},
+        signs{std::forward<Signs>(signs)},
+        last_var{std::forward<LastVar>(last_var)} {}
+
+  /**
+   * @brief Construct the instance from a formula.
+   * @tparam Formula An iterable type with elements of type Clause.
+   *  A Clause is an iterable type with elements of type Literal.
+   *  A Literal is a type that can be cast to a signed integer.
+   * @param formula An object of type Formula.
+   */
+  template <typename Formula>
+  explicit Instance(const Formula &formula) : Instance{_Build(formula)} {}
+
+  std::size_t n_vars() const {
+    /*
+     * Return number of variables.
+     */
+    assert(std::size(clauses) == std::size(signs));
+    return std::size(clauses);
+  }
+
+  std::size_t n_clauses() const {
+    /*
+     * Return number of clauses.
+     */
+    return std::size(last_var);
+  }
+
+  bool operator==(const Instance &other) const {
+    /*
+     * Check equality.
+     */
+    return clauses == other.clauses && signs == other.signs &&
+           last_var == other.last_var;
+  }
+
+private:
+  template <typename Formula>
+  static constexpr auto _Build(const Formula &formula) {
+    // Check all variables are different from zero
+    for (const auto &cl_ : formula)
+      for (const auto &x_ : cl_)
+        if (x_ == 0)
+          throw std::runtime_error(
+              "All variables should be different from zero");
+
+    // Get number of variables
+    const auto n_ = GetNVars(formula);
+
+    // Get number of constraints
+    const auto m_ = std::size(formula);
+
+    // Initialize
+    std::decay_t<decltype(clauses)> clauses_(n_);
+    std::decay_t<decltype(signs)> signs_(n_);
+    std::decay_t<decltype(last_var)> last_var_;
+
+    // Update clauses and signs
+    for (std::size_t i_ = 0; i_ < m_; ++i_) {
+      // Get clause
+      const auto &clause_ = formula[i_];
+
+      // For each variable
+      for (const auto &x_ : clause_) {
+        // Get position of variable
+        std::size_t pos_ = std::abs(x_) - 1;
+
+        // Check
+        if (pos_ >= n_)
+          throw std::runtime_error(
+              "Variable index is larger than the number of variables");
+
+        // Update clauses
+        clauses_[pos_].push_back(i_);
+
+        // Update signs
+        signs_[pos_].push_back(x_ < 0);
+      }
+    }
+
+    // Update last variable
+    for (const auto &clause_ : formula)
+      last_var_.push_back(std::transform_reduce(
+                              std::begin(clause_), std::end(clause_), 0,
+                              [](auto &&x, auto &&y) { return std::max(x, y); },
+                              [](auto &&x) { return std::abs(x); }) -
+                          1);
+
+    // Return new instance
+    return Instance{clauses_, signs_, last_var_};
+  }
+};
+} // namespace pysa::dpll::sat
