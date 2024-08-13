5bc526614d96605966b2b305edc2f282b96ab00d
hmunozb <humberto.munozb@gmail.com>
2024-08-13 00:35:51 -0700

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <iostream>
 #include <filesystem>
 #include <fstream>
@@ -12,6 +13,7 @@ int main(int argc, char* argv[]) {
                            << " cnf_file" << std::endl;
   }
 
+  auto t0_ = std::chrono::high_resolution_clock::now();
   std::string cnf_file{argv[1]};
   std::ifstream ifs(cnf_file);
   if(!ifs.good()){
@@ -21,7 +23,11 @@ int main(int argc, char* argv[]) {
   FormulaT formula = pysa::algstd::ReadCNF(ifs);
   //std::cout << formula << std::endl;
   CDCL cdcl(std::move(formula));
+
+  auto it_ = std::chrono::high_resolution_clock::now();
   int rc = cdcl.run();
+  auto et_ = std::chrono::high_resolution_clock::now();
+
   if (rc == CDCLSAT) {
     for (uint8_t b: cdcl.prop._state) {
       std::cout << int(b);
@@ -32,4 +38,13 @@ int main(int argc, char* argv[]) {
   } else {
     std::cerr << rc << "\n";
   }
+  std::cout << "Pre-processing time "
+            << std::chrono::duration_cast<std::chrono::microseconds>(it_ - t0_)
+                .count() << " us\n";
+  std::cout << "Computation time: "
+            << std::chrono::duration_cast<std::chrono::microseconds>(et_ - it_)
+                .count() << " us\n";
+  std::cout << "Total time: "
+            << std::chrono::duration_cast<std::chrono::microseconds>(et_ - t0_)
+                .count() << " us\n";
 }
